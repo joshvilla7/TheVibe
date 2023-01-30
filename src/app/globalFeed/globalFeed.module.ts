3535d042c5c6dc5097e0e43e1086c464c2f9068f
joshvilla <joshvilla777@gmail.com>
2023-01-30 13:08:40 -0500

    added feed toggler component, for user to navigate between global and own feed

--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { BannerModule } from "../shared/types/modules/banner/banner.module";
 import { FeedModule } from "../shared/types/modules/feed/feed.module";
+import { FeedTogglerModule } from "../shared/types/modules/feedToggler/feedToggler.module";
 import { PopularTagsModule } from "../shared/types/modules/popularTags/popularTags.module";
 import { GlobalFeedComponent } from "./components/globalFeed.component";
 
@@ -11,7 +12,14 @@ const routes = [
 ]
 
 @NgModule({
-    imports: [CommonModule, RouterModule.forChild(routes), FeedModule, BannerModule, PopularTagsModule],
+    imports: [
+        CommonModule, 
+        RouterModule.forChild(routes), 
+        FeedModule, 
+        BannerModule, 
+        PopularTagsModule, 
+        FeedTogglerModule
+    ],
     declarations: [GlobalFeedComponent]
 })
 export class GlobalFeedModule {}

6bf9667255d84d57fd2c09e7410b1af582ca7521
joshvilla <joshvilla777@gmail.com>
2023-02-03 11:46:28 -0500

    fixed article component to show articles properly

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
+import { ErrorMessageModule } from "../shared/types/modules/errorMessage/errorMessage.module";
+import { LoadingModule } from "../shared/types/modules/loading/loading.module";
+import { TagListModule } from "../shared/types/modules/tagList/tagList.module";
 import { ArticleService } from "../shared/types/services/article.service";
 import { ArticleComponent } from "./components/article.component";
 import { GetArticleEffect } from "./store/effects/getArticle.effect";
@@ -17,7 +20,10 @@ const routes = [
         CommonModule,
         EffectsModule.forFeature([GetArticleEffect]),
         StoreModule.forFeature('article', reducers),
-        RouterModule.forChild(routes)
+        RouterModule.forChild(routes),
+        LoadingModule,
+        ErrorMessageModule,
+        TagListModule
     ],
     declarations: [ArticleComponent],
     exports: [],

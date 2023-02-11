e0a067fa11b2682c95362efd6f13c56fff2ad28a
joshvilla <joshvilla777@gmail.com>
2023-02-11 17:24:45 -0500

    fixing userProfile component

--- src/app/userProfile/components/userProfile.component.ts
+++ src/app/userProfile/components/userProfile.component.ts
@@ -1,7 +1,77 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from '@angular/core'
+import {Observable, Subscription, combineLatest} from 'rxjs'
+import {Store, select} from '@ngrx/store'
+import {ActivatedRoute, Router, Params} from '@angular/router'
+
+import {currentUserSelector} from 'src/app/auth/store/selectors'
+import {map, filter} from 'rxjs/operators'
+import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
+import { getUserProfileAction } from '../store/actions/getUserProfile.action'
+import { errorSelector, isLoadingSelector, userProfileSelector } from '../store/selectors'
+import { UserProfileInterface } from '../types/userProfile.interface'
 
 @Component({
-    selector: 'mc-user-profile',
-    templateUrl: './userProfile.component.html'
+  selector: 'mc-user-profile',
+  templateUrl: './userProfile.component.html',
 })
-export class UserProfileComponent {}
+export class UserProfileComponent implements OnInit {
+  userProfile: UserProfileInterface
+  isLoading$: Observable<boolean>
+  error$: Observable<string | null>
+  userProfileSubscription: Subscription
+  slug: string
+  isCurrentUserProfile$: Observable<boolean>
+
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.initializeValues()
+    this.initializeListeners()
+  }
+  initializeValues(): void {
+    this.slug = this.route.snapshot.paramMap.get('slug')
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector))
+    this.error$ = this.store.pipe(select(errorSelector))
+    this.isCurrentUserProfile$ = combineLatest(
+      this.store.pipe(select(currentUserSelector), filter(Boolean)),
+      this.store.pipe(select(userProfileSelector), filter(Boolean))
+    ).pipe(
+      map(
+        ([currentUser, userProfile]: [
+          CurrentUserInterface,
+          UserProfileInterface
+        ]) => {
+          return currentUser.username === userProfile.username
+        }
+      )
+    )
+  }
+
+  getApiUrl(): string {
+    const isFavorites = this.router.url.includes('favorites')
+    return isFavorites
+      ? `/articles?favorited=${this.slug}`
+      : `/articles?author=${this.slug}`
+  }
+
+  initializeListeners(): void {
+    this.userProfileSubscription = this.store
+      .pipe(select(userProfileSelector))
+      .subscribe((userProfile: UserProfileInterface) => {
+        this.userProfile = userProfile
+      })
+
+    this.route.params.subscribe((params: Params) => {
+      this.slug = params.slug
+      this.fetchUserProfile()
+    })
+  }
+
+  fetchUserProfile(): void {
+    this.store.dispatch(getUserProfileAction({slug: this.slug}))
+  }
+}
--- src/app/userProfile/userProfile.module.ts
+++ src/app/userProfile/userProfile.module.ts
@@ -1,26 +1,34 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { EffectsModule } from "@ngrx/effects";
-import { StoreModule } from "@ngrx/store";
-import { UserProfileComponent } from "./components/userProfile.component";
-import { UserProfileService } from "./service/userProfile.service";
-import { GetUserProfileEffect } from "./store/effects/userProfile.effect";
-import { reducers } from "./store/reducers";
+import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {RouterModule} from '@angular/router'
+import {UserProfileComponent} from 'src/app/userProfile/components/userProfile.component'
+import {EffectsModule} from '@ngrx/effects'
+import {StoreModule} from '@ngrx/store'
+import {reducers} from 'src/app/userProfile/store/reducers'
+import { UserProfileService } from './service/userProfile.service'
+import { FeedModule } from '../shared/types/modules/feed/feed.module'
+import { GetUserProfileEffect } from './store/effects/userProfile.effect'
 
 const routes = [
-    { path: 'profiles/:slug', commponent: UserProfileComponent},
-    { path: 'profiles/:slug/favorites', component: UserProfileComponent}
+  {
+    path: 'profiles/:slug',
+    component: UserProfileComponent
+  },
+  {
+    path: 'profiles/:slug/favorites',
+    component: UserProfileComponent
+  }
 ]
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule.forChild(routes),
-        EffectsModule.forFeature([GetUserProfileEffect]),
-        StoreModule.forFeature('userProfile', reducers)
-    ],
-    declarations: [UserProfileComponent],
-    providers: [UserProfileService]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    EffectsModule.forFeature([GetUserProfileEffect]),
+    StoreModule.forFeature('userProfile', reducers),
+    FeedModule
+  ],
+  declarations: [UserProfileComponent],
+  providers: [UserProfileService]
 })
 export class UserProfileModule {}
